d63661153377260740d11037ca52ae6c7d1bf628
Daniel Kanevsky <dk19dk07@gmail.com>
2021-09-07 16:05:05 +0300

    add ajax and canvas

---
+++ BlueLibrary/wwwroot/js/get_advice.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(() => {
+    $.ajax({
+        type: "GET",
+        url: "https://api.adviceslip.com/advice",
+        dataType: "json"
+    }).done((response) => {
+        var text = response.slip.advice;
+        const canvas = $("#canvas-tip")[0];
+        canvas.width = 800;
+        var ctx = canvas.getContext('2d');
+        ctx.shadowColor = "lemonchiffon";
+        ctx.shadowOffsetX = 10;
+        ctx.shadowOffsetY = 10
+        ctx.shadowBlur = 10;
+        ctx.font = "25px arial";
+        var gradient = ctx.createLinearGradient(0, 0, 150, 100);
+        gradient.addColorStop(0, "rgb(255, 0, 128)");
+        gradient.addColorStop(1, "rgb(255, 153, 51)");
+        ctx.fillStyle = gradient;
+        ctx.fillText(text, 10, 50);
+    });
+});
