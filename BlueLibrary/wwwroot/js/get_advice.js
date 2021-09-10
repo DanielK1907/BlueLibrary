0811a65ec49c84ef3e832557b67c2833d1e769b7
Daniel Kanevsky <dk19dk07@gmail.com>
2021-09-10 14:45:39 +0300

    refactor: change get_advice.js so it matches ES6 standards

--- BlueLibrary/wwwroot/js/get_advice.js
+++ BlueLibrary/wwwroot/js/get_advice.js
@@ -4,16 +4,16 @@
         url: "https://api.adviceslip.com/advice",
         dataType: "json"
     }).done((response) => {
-        var text = response.slip.advice;
+        const text = response.slip.advice;
         const canvas = $("#canvas-tip")[0];
         canvas.width = 1600;
-        var ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d');
         ctx.shadowColor = "lemonchiffon";
         ctx.shadowOffsetX = 10;
         ctx.shadowOffsetY = 10
         ctx.shadowBlur = 10;
         ctx.font = "25px arial";
-        var gradient = ctx.createLinearGradient(0, 0, 150, 100);
+        const gradient = ctx.createLinearGradient(0, 0, 150, 100);
         gradient.addColorStop(0, "rgb(255, 0, 128)");
         gradient.addColorStop(1, "rgb(255, 153, 51)");
         ctx.fillStyle = gradient;
