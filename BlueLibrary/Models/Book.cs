e06172227f582fcef6f66abab28894e5a20ca345
lironyosef11 <lironyosef11@gmail.com>
2021-09-11 20:06:49 +0300

    Added some more validations for book fields

--- BlueLibrary/Models/Book.cs
+++ BlueLibrary/Models/Book.cs
@@ -11,15 +11,21 @@ namespace BlueLibrary.Models
 
         [Required]
         [DataType(DataType.Text)]
+        [RegularExpression(@"^[aA-zZ0-9][aA-zZ0-9\s]{0,14}$",
+         ErrorMessage = "Name must contain up to 15 english characters and digits.")]
         public String BookName { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
+        [RegularExpression(@"^[aA-zZ][aA-zZ\s]{0,14}$",
+         ErrorMessage = "Author must contain up to 15 english characters.")]
         public String Author { get; set; }
         public DateTime? ReleaseDate { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
+        [RegularExpression(@"^[aA-zZ0-9][aA-zZ0-9\s]{0,49}$",
+         ErrorMessage = "Description must contain up to 50 english characters and digits.")]
         public String Description { get; set; }
         
         [ForeignKey("BookImage")]
